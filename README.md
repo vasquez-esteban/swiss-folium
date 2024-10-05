a90b7de381489b6f431268f6b8b98053d95a5488
vasquez-esteban <evasquezp@unal.edu.co>
2024-10-04 20:25:35 -0500

    Initialize Repo with template

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# swiss-folium
-Portfolio Site
+# Initial Repository for Astro Projects
+
+This repository provides a base start point for Astro Projects.
+
+It includes:
+
+- Basic configuration with TS and Tailwind.
+- Extended configuration for Linting and Formatting with Eslint and Prettier.
+- Custom Swiper Slider.
+- Custom Accordion made with Astro.
+
+(It is recommended to add formatting support using VScode astro plugin & prettier, alongside with Headwind for tailwind sorting)
+
+## Requirements
+
+- Node 22.2.0 or 20.13.1
+- pnpm 9.1.2
+- others in package.json
+
+## Deps
+
+run `pnpm i`
+
+# Usage
+
+- `npm run lint` to use eslint
+- `npm run dev` to launch dev environment
---
+++ tailwind.config.mjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        "primary-foreground": "var(--primary-foreground)",
+        "secondary-foreground": "var(--secondary-foreground)",
+        "muted-foreground": "var(--muted-foreground)",
+        accent: "var(--accent)",
+        "accent-foreground": "var(--accent-foreground)",
+        destructive: "var(--destructive)",
+        "destructive-foreground": "var(--destructive-foreground)",
+        border: "var(--border)",
+        input: "var(--input)",
+        ring: "var(--ring)",
+      },
+      borderRadius: {
+        DEFAULT: "var(--radius)",
+      },
+    },
+  },
+  plugins: [],
+};
